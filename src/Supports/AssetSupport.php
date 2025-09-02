091b971d9b1b235b6276e26a788b82ac9067940b
JadeSass <pelumiezekiel.developer@gmail.com>
2025-09-02 15:23:53 +0100

    Append build verion for assets using php unix time method

---
+++ src/Supports/AssetSupport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kernery\Asset\Supports;
+
+use Illuminate\Config\Repository;
+
+class AssetSupport
+{
+    protected array $config = [];
+    protected array $styles = [];
+    protected array $scripts = [];
+    protected string $assetBuildVersion = "";
+
+    public function __construct(Repository $config)
+    {
+        $this->config =   $config->get('asset');
+        $this->styles = $config->get('asset_styles');
+        $this->scripts = $config->get('asset_scripts');
+    }
+
+    /**
+     * Append build verion for assets
+     */
+    public function getAssetBuildVersion(): string
+    {
+        $buildVersion = $this->assetBuildVersion = $this->config['allow_assets_version'] ? '?v=' . $this->config['assets_version'] : '';
+
+        return $buildVersion;
+    }
+}
