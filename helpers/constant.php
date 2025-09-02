3c3ff6123d8fc8a73091810aa5d6afd6af955a35
JadeSass <pelumiezekiel.developer@gmail.com>
2025-09-02 14:38:19 +0100

    Declaring hooks constant in helper

---
+++ helpers/constant.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined("ASSET_SCRIPTS_HEADER")) {
+    define("ASSET_SCRIPTS_HEADER","asset_scripts_header");
+}
+
+if (!defined("ASSET_SCRIPTS_FOOTER")) {
+    define("ASSET_SCRIPTS_FOOTER", "asset_scripts_footer");
+}
+
+if (!defined("ASSET_STYLES_HEADER")) {
+    define("ASSET_STYLES_HEADER", "asset_styles_header");
+}
+
+if (!defined("ASSET_STYLES_FOOTER")) {
+    define("ASSET_STYLES_FOOTER", "asset_styles_footer");
+}
