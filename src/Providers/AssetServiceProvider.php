d740afa9418b2e2537e8f3580b37fa3fcfbf5a27
JadeSass <pelumiezekiel.developer@gmail.com>
2025-09-02 15:04:08 +0100

    Configured service providers to publish views and config

---
+++ src/Providers/AssetServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kernery\Asset\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssetServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . "/../../resources/views", "asset");
+        $this->mergeConfigFrom(__DIR__ . "/../../config/global", "asset");
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__ . "/../../resources/views" => resource_path("views/vendor/asset")], "views");
+            $this->publishes([__DIR__ . "/../../config/global" => config_path("global.php")], "config");
+        }
+    }
+}
