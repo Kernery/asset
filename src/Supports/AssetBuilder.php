6edfeeb23e11bb668a3662246232914a10cc6e08
JadeSass <pelumiezekiel.developer@gmail.com>
2025-09-03 10:48:05 +0100

    update_fix: Removed link script builder for script url

--- src/Supports/AssetBuilder.php
+++ src/Supports/AssetBuilder.php
@@ -3,21 +3,11 @@
 namespace Kernery\Asset\Supports;
 
 use Illuminate\Contracts\Routing\UrlGenerator;
-use Illuminate\Support\HtmlString;
 
 class AssetBuilder
 {
     public function __construct(UrlGenerator $urlGenerator)
     {
         $this->url = $urlGenerator;
-    }
-
-    public function linkScript(string $url, array $attributes = [], ?bool $secure_asset = null): string
-    {
-        // return empty html string if no url
-        if (! $url) {
-            return new HtmlString();
-        }
-
     }
 }
