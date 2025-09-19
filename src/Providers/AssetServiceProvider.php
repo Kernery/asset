6148d98ee1ff3122a3c4e441c9ddb1ff4c984ed2
Kernery <kernerylimited@gmail.com>
2025-09-19 17:55:57 +0100

    update: asset service

--- src/Providers/AssetServiceProvider.php
+++ src/Providers/AssetServiceProvider.php
@@ -3,17 +3,16 @@
 namespace Kernery\Asset\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Kernery\Main\Traits\LoadAndPublishDataTrait;
 
 class AssetServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'asset');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/global.php', 'asset');
+    use LoadAndPublishDataTrait;
 
-        if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/asset')], 'views');
-            $this->publishes([__DIR__ . '/../../config/global' => config_path('global.php')], 'config');
-        }
+    public function register(): void
+    {
+        $this->setNamespace('core/asset')
+            ->loadAndPublishConfigs('global')
+            ->loadHelpers();
     }
 }
